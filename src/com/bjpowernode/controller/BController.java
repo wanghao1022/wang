b78e73921f8d36b3597a903f8c1efb1a136d4e1b
wanghao <1041914439@qq.com>
2020-12-16 17:14:46 +0800

    测试push和pull

---
+++ src/com/bjpowernode/controller/BController.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.controller;
+
+/**
+ * @ProjectName: wang
+ * @Package: com.bjpowernode.controller
+ * @Description: java类作用描述
+ * @Author: 王浩
+ * @CreateDate: 2020/12/16 17:13
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class BController {
+    //第二个测试类
+}
