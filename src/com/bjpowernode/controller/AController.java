44910fb4e7758e2207e141e19bfb1a378665d72e
wanghao <1041914439@qq.com>
2020-12-16 16:55:06 +0800

    push测试类

---
+++ src/com/bjpowernode/controller/AController.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.controller;
+
+/**
+ * @ProjectName: 01-ssm
+ * @Package: com.bjpowernode.controller
+ * @Description: java类作用描述
+ * @Author: 王浩
+ * @CreateDate: 2020/12/16 16:54
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class AController {
+    //测试类
+}
