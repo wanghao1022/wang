fe94beb5ebaa8f999e8a11ff5ab712062252bbc6
ASUS <1041914439@qq.com>
2021-05-13 21:36:28 +0800

    这是一个测试类

---
+++ src/com/bjpowernode/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.controller;
+
+/**
+ * @ProjectName: wang
+ * @Package: com.bjpowernode.controller
+ * @Description: java类作用描述
+ * @Author: 王浩
+ * @CreateDate: 2021/5/13 21:35
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2021
+ */
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("这是一个测试类");
+    }
+}
